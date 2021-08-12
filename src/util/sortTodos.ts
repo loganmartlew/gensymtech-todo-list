e18d69c4f1a4f2edcffd5441c4474674e4c398f2
Logan Martlew <logan.martlew@gmail.com>
2021-08-12 23:30:47 +1200

    Completed todos are sorted separately and displayed below incomplete todos

--- src/util/sortTodos.ts
+++ src/util/sortTodos.ts
@@ -1,21 +1,27 @@
 import Todo from '../types/Todo';
 
+const compareTodos = (a: Todo, b: Todo) => {
+  const orderedA = {
+    ...a,
+    order: a.isOrdered ? a.order! : Number.MAX_SAFE_INTEGER,
+  };
+
+  const orderedB = {
+    ...b,
+    order: b.isOrdered ? b.order! : Number.MAX_SAFE_INTEGER,
+  };
+
+  return orderedA.order - orderedB.order;
+};
+
 const sortTodos = (todos: Todo[]) => {
-  const sortedTodos = todos.sort((a, b) => {
-    const orderedA = {
-      ...a,
-      order: a.isOrdered ? a.order! : Number.MAX_SAFE_INTEGER,
-    };
+  const incompleteTodos = todos.filter(todo => !todo.complete);
+  const completeTodos = todos.filter(todo => todo.complete);
 
-    const orderedB = {
-      ...b,
-      order: b.isOrdered ? b.order! : Number.MAX_SAFE_INTEGER,
-    };
+  const sortedIncompleteTodos = incompleteTodos.sort(compareTodos);
+  const sortedCompleteTodos = completeTodos.sort(compareTodos);
 
-    return orderedA.order - orderedB.order;
-  });
-
-  return sortedTodos;
+  return [...sortedIncompleteTodos, ...sortedCompleteTodos];
 };
 
 export default sortTodos;
